b8bfcd0a8413b35a1d109402681f4efc9c12827b
Andrii-Velhan <velganaa@gmail.com>
2020-11-21 17:17:07 +0200

    add base data to apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,9 +1,17 @@
-const BASE_URL = `https://restcountries.eu/rest/v2`;
+const BASE_URL = `https://pixabay.com/api/`;
+const MY_KEY = `19199733-53a137615acbd00e25277177c`;
+let page = 1;
+const per_page = 12;
     
 
-function fetch(searchQuery) {
-    return fetch(`${BASE_URL}/name/${searchQuery}`)
-        .then(response => response.json()); 
+function fetchApiPictures(searchQuery) {
+    return fetch(`${BASE_URL}?image_type=photo
+    &orientation=horizontal
+    &q=${searchQuery}
+    &q=${page}
+    &per_page=${per_page}
+    &key=${MY_KEY}`)
+        .then(r => r.json()); 
 }
 
-export default { fetchCountries };
+export default { fetchApiPictures };
